b723ede32f328f4bbecaded08aeee6bdb199df19
zondimaqina <zondimaqina0@gmail.com>
2024-05-05 16:25:24 +0200

    add class and instance

---
+++ sub-strings/main.rb
@@ -0,0 +1,12 @@
+# get user sentence and dictionary
+# list all words used in user sentnce
+# compare dictionary list and user sentnce list
+# return number of equal strings from user sentnece and dictionary
+
+class SubString
+  def initialize(user_sentence)
+    @dictionary = ['hello', 'hell', 'world']
+  end
+end
+
+count_words = SubString.new()
