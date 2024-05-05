269d244437b2e87b7d08daa823c5bc6cfb0b1b17
zondimaqina <zondimaqina0@gmail.com>
2024-05-05 19:42:56 +0200

    include letters

--- ceasor-cypher/main.rb
+++ ceasor-cypher/main.rb
@@ -1,14 +1,19 @@
 # take user sentence
-# switch each letter in the user sentence 
+# switch each letter in the user sentence
 # return new string of switched letters
 
 class CeasorCypher
   def initialize(sentence, shifter)
     @user_input = sentence
     @shift = shifter
+    @letters = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+  end
+
+  def cypher
+    
   end
 end
 
-string_input = "ab"
+string_input = 'ab'
 shifter = 2
 input_shift = CeasorCypher.new(string_input, shifter)
