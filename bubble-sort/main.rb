01b5ab34ef266055d0c013f20b7dc7cf0b286f20
zondimaqina <zondimaqina0@gmail.com>
2024-05-06 10:38:55 +0200

    finish bubble-sort project

---
+++ bubble-sort/main.rb
@@ -0,0 +1,21 @@
+class BubbleSort
+  def initialize(list)
+    @list = list
+  end
+
+  def sort
+    int = @list.length - 1
+
+    for i in 0...int # iterate through all integers of array to compare
+        if @list[i] > @list[i + 1]
+           @list[i], @list[i + 1] = @list[i + 1], @list[i] # rearrange each time
+        end
+    end 
+    print @list  
+  end
+end
+
+list = [2, 1, 3, 10, 5]
+integer_list = BubbleSort.new(list) # take array of numbers
+
+integer_list.sort
